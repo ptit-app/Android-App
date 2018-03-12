4372eddb8b1d0b369c242d72bf38acc1287b6a88
jakenguyen1206 <nguyenphuocduy1212@gmail.com>
2018-03-12 13:19:07 +0700

    Duy Tao Activity Nhe <3

---
+++ Project/app/src/main/java/ptit/ntnt/ptitapp/TeacherRatingActivity.java
@@ -0,0 +1,13 @@
+package ptit.ntnt.ptitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherRatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacherrating);
+    }
+}
