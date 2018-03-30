e38627c16e80036d1a3e944c122246f63f7565ce
Dat Shiro <datshiro@gmail.com>
2018-03-30 08:17:34 +0700

    Update firebase methods

--- Project/app/src/main/java/ptit/ntnt/ptitapp/Firebase/FirebaseHelper.java
+++ Project/app/src/main/java/ptit/ntnt/ptitapp/Firebase/FirebaseHelper.java
@@ -9,9 +9,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import ptit.ntnt.ptitapp.Database.DBConst;
+import ptit.ntnt.ptitapp.Models.Course;
 import ptit.ntnt.ptitapp.Models.Lecturer;
+import ptit.ntnt.ptitapp.Models.News;
+import ptit.ntnt.ptitapp.Models.PTITClass;
+import ptit.ntnt.ptitapp.Models.Schedule;
+import ptit.ntnt.ptitapp.Models.Subject;
 import ptit.ntnt.ptitapp.Models.Student;
 import ptit.ntnt.ptitapp.Models.User;
+import ptit.ntnt.ptitapp.Models.UserGroup;
 import ptit.ntnt.ptitapp.R;
 
 /**
@@ -25,9 +31,9 @@ public class FirebaseHelper {
         mData = FirebaseDatabase.getInstance().getReference();
     }
 
-    public Student getStudent (String StudentID){
+    public Student getStudent (String studentID){
         final Student[] students = {new Student()};
-        mData.child(DBConst.TB_STUDENT.TB_NAME).child(StudentID).addListenerForSingleValueEvent(new ValueEventListener() {
+        mData.child(DBConst.TB_STUDENT.TB_NAME).child(studentID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 students[0] = dataSnapshot.getValue(Student.class);         // Do cai ham nay doi` phai? tra ve` 1 phan tu cua mang
@@ -46,9 +52,9 @@ public class FirebaseHelper {
         }
     }
 
-    public Lecturer getLecturer(String LecturerID){
+    public Lecturer getLecturer(String lecturerID){
         final Lecturer[] lecturers = {new Lecturer()};
-        mData.child(DBConst.TB_STUDENT.TB_NAME).child(LecturerID).addListenerForSingleValueEvent(new ValueEventListener() {
+        mData.child(DBConst.TB_LECTURER.TB_NAME).child(lecturerID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 lecturers[0] = dataSnapshot.getValue(Lecturer.class);         // Do cai ham nay doi` phai? tra ve` 1 phan tu cua mang
@@ -64,6 +70,137 @@ public class FirebaseHelper {
             return lecturers[0];
         }else{
             return null ;
+        }
+    }
+
+    public Subject getSubject(String subjectID){
+        final Subject[] subjects = {new Subject()};
+        mData.child(DBConst.TB_SUBJECT.TB_NAME).child(subjectID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                subjects[0] = dataSnapshot.getValue(Subject.class);         // Do cai ham nay doi` phai? tra ve` 1 phan tu cua mang
+                Log.d("Subject", subjects[0].toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        if (subjects[0] != null){
+            return subjects[0];
+        }else{
+            return null ;
+        }
+    }
+
+    public Course getCourse(final String courseID){
+        final Course[] courses = {new Course()};
+        mData.child(DBConst.TB_COURSE.TB_NAME).child(courseID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                courses[0] = dataSnapshot.getValue(Course.class);
+                Log.d("Course", courses[0].toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if (courses[0] != null){
+            return courses[0];
+        }else{
+            return null;
+        }
+    }
+
+    public Schedule getSchedule(final String scheduleID){
+        final Schedule[] schedules = {new Schedule()};
+        mData.child(DBConst.TB_SCHEDULE.TB_NAME).child(scheduleID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                schedules[0] = dataSnapshot.getValue(Schedule.class);
+                Log.d("Schedule", schedules[0].toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if (schedules[0] != null){
+            return schedules[0];
+        }else{
+            return null;
+        }
+    }
+
+    public PTITClass getPTITClass(final String ptitClassID){
+        final PTITClass[] ptitClasses = {new PTITClass()};
+        mData.child(DBConst.TB_PTIT_CLASS.TB_NAME).child(ptitClassID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ptitClasses[0] = dataSnapshot.getValue(PTITClass.class);
+                Log.d("PTITClass", ptitClasses[0].toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if (ptitClasses[0] != null){
+            return ptitClasses[0];
+        }else{
+            return null;
+        }
+    }
+
+    public News getNews(final String newsID){
+        final News[] news = {new News()};
+        mData.child(DBConst.TB_NEWS.TB_NAME).child(newsID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                news[0] = dataSnapshot.getValue(News.class);
+                Log.d("News", news[0].toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if (news[0] != null){
+            return news[0];
+        }else{
+            return null;
+        }
+    }
+
+    public UserGroup getUserGroup(final String userGroupName){
+        final UserGroup[] userGroups = {new UserGroup()};
+        mData.child(DBConst.TB_USER_GROUP.TB_NAME).child(userGroupName).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userGroups[0] = dataSnapshot.getValue(UserGroup.class);
+                Log.d("UserGroup", userGroups[0].toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if (userGroups[0] != null){
+            return userGroups[0];
+        }else{
+            return null;
         }
     }
 
